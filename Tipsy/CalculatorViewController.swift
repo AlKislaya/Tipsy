cbe34ddebf94904fdd91efbf1770f9188d12e311
alkislaya <41301020+AlKislaya@users.noreply.github.com>
2025-10-15 19:42:09 +0300

    Calculator layout

---
+++ Tipsy/CalculatorViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Alexandra on 15.10.25.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPrcButton: UIButton!
+    @IBOutlet weak var tenPrcButton: UIButton!
+    @IBOutlet weak var twentyPrcButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+}
+
